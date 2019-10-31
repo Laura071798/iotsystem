8451154191de4a48a1cee97b812a725515f89675
Laura <laura.ramirez2@correo.usa.edu.co>
2019-10-31 12:15:09 -0500

    add .gitignore add 3 buttons and adc

--- tkinterbd.py
+++ tkinterbd.py
@@ -26,10 +26,10 @@ ventana.geometry('1280x800')
 ventana.title("UI para sistemas de control")
 
 # Fetch the service account key JSON file contents
-cred = credentials.Certificate('keys/key.json')
+cred = credentials.Certificate('key/key.json')
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://bdtkinter.firebaseio.com/'
+    'databaseURL': 'https://iotsystem-b1ecc.firebaseio.com/'
 })
 
 
@@ -42,28 +42,65 @@ photoImg=  ImageTk.PhotoImage(img)
 b.configure(image=photoImg)
 b.place(x = 760,y = 20)
 
-valor= Label(marco1, text="0.0,", bg='cadet blue1', font=("Arial Bold", 15), fg="white", width=5)
+valor= Label(marco1, bg='cadet blue1', font=("Arial Bold", 15), fg="white", width=5)
 variable=StringVar()
+valor2= Label(marco1, bg='cadet blue1', font=("Arial Bold", 15), fg="white", width=5)
+adc_data=StringVar()
+
+
 def update_label():
     global cont
     cont=cont+1
-    ref = db.reference("/")
-    ref.set({'sensor': 
-            {
-              'valor': cont,
-            
+    ref = db.reference("sensor")
+    ref.update({
+                'sensor1': {
+                    'adc': 0,
+                    'valor': cont,
+                    
             }
-        })
+         })
     variable.set(cont)
+
+def adc_read():
+    global prom
+    i=0
+    prom=0
+    while i<15:
+        i=i+1
+        x=a_0.read()
+        print(x)
+        adc_data.set(x)
+        prom=x+prom
+        ventana.update()
+        time.sleep(0.1)
+    prom=prom/15
+    print("El promedio es ",prom)
+    ref = db.reference('sensor')
+    ref.update({
+        'sensor2/adc': prom
+    })
+
+def save():
+    ref = db.reference('sensor')
+    ref.update({
+        'sensor3/message': 'hola'
+    })
     
+    
+  
 
 valor.configure(textvariable=variable)
 valor.place(x=20, y=90)
-start_button=Button(ventana,text="start",command=update_label)
+start_button=Button(marco1,text="cont",command=update_label)
 start_button.place(x=20, y=160)
 
+valor2.configure(textvariable=adc_data)
+valor2.place(x=130, y=90)
+start_button2=Button(marco1,text="adc_data",command=adc_read)
+start_button2.place(x=80, y=160)
 
-
+save_button=Button(marco1,text="save",command=save)
+save_button.place(x=170, y=160)
 
 
 ventana.mainloop()
